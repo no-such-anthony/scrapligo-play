2a6ba145f6475ae23efd167fc27d08146054920e
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2022-01-09 17:32:18 +1000

    create workerpool1

---
+++ workerpool1/main.go
@@ -0,0 +1,131 @@
+package main
+
+// based on code found at https://github.com/PacktPublishing/Network-Automation-with-Go
+
+import (
+	"fmt"
+	"time"
+	"github.com/scrapli/scrapligo/driver/base"
+	"github.com/scrapli/scrapligo/driver/core"
+)
+
+type Host struct {
+	Name      string
+	Hostname  string
+	Platform  string
+	Username  string
+	Password  string
+	Enable    string
+	StrictKey bool
+	Data      map[string]interface{}
+}
+
+type Hosts map[string]Host
+
+
+func timeTrack(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("This process took %s\n", elapsed)
+}
+
+func getVersion(h Host) {
+	d, err := core.NewCoreDriver(
+		h.Hostname,
+		h.Platform,
+		base.WithAuthStrictKey(h.StrictKey),
+		base.WithAuthUsername(h.Username),
+		base.WithAuthPassword(h.Password),
+		//base.WithTransportType("standard"),
+		//base.WithSSHConfigFile("ssh_config"),
+	)
+
+	if err != nil {
+		fmt.Printf("failed to create driver for %s: %+v\n\n", h.Hostname, err)
+		return
+	}
+
+	err = d.Open()
+	if err != nil {
+		fmt.Printf("failed to open driver for %s: %+v\n\n", h.Hostname, err)
+		return
+	}
+	defer d.Close()
+
+	rs, err := d.SendCommand("show version")
+	if err != nil {
+		fmt.Printf("failed to send command for %s: %+v\n\n", h.Hostname, err)
+		return
+	}
+
+	parsedOut, err := rs.TextFsmParse("../textfsm_templates/" + h.Platform + "_show_version.textfsm")
+	if err != nil {
+		fmt.Printf("failed to parse command for %s: %+v\n\n", h.Hostname, err)
+		return
+	}
+
+	fmt.Printf("Hostname: %s\nHardware: %s\nSW Version: %s\nUptime: %s\n\n",
+				h.Hostname, parsedOut[0]["HARDWARE"],
+				parsedOut[0]["VERSION"], parsedOut[0]["UPTIME"])
+
+}
+
+func getHosts() Hosts {
+
+	devices := []string{"no.suchdomain","192.168.204.101","192.168.204.102","192.168.204.103","192.168.204.104"}
+
+	hosts := make(Hosts)
+
+	for _,value := range devices {
+		var host Host
+		host.Data = make(map[string]interface{})
+
+		host.Name = value
+		host.Hostname = value
+		host.Platform = "cisco_iosxe"
+		host.Username = "fred"
+		host.Password = "bedrock"
+		host.Data["example_only"] = 100
+
+		hosts[host.Name] = host
+		
+	}
+
+	return hosts
+}
+
+func worker(host_jobs <-chan Host, done chan<- bool) {
+	for h := range host_jobs {
+		getVersion(h)
+		done <- true
+	}
+}
+
+func main() {
+	// To time this process
+	defer timeTrack(time.Now())
+
+	hosts := getHosts()
+	//fmt.Println(hosts)
+
+	//Worker Pools! 
+	//Let's try with just a simple done return channel before we try with a results return channel
+
+	const num_workers = 3
+    host_jobs := make(chan Host, 100)
+	done := make(chan bool, 100)
+
+	for w := 1; w <= num_workers; w++ {
+		go worker(host_jobs, done)
+    }
+
+	for _, host := range hosts {
+        host_jobs <- host
+    }
+    close(host_jobs)
+
+	for r := 1; r <= len(hosts); r++ {
+		<-done
+	}
+	
+
+}
